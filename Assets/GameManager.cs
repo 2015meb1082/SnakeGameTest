d8ffe7d6d666b079cfeac6f6827404993c1b0e6e
2015meb1082 <abhishek.sainiss441998@gmail.com>
2020-09-11 08:33:55 +0530

    Game Manager added with game pause and resume functionality.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,7 +1,50 @@
 ﻿using UnityEngine;
-
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    
+    [SerializeField]
+    private GameObject gameOverPanel;
+    [SerializeField]
+    private GameObject inGamePanel;
+    [SerializeField]
+    private GameObject pausePanel;
+
+    public int score;
+    public bool gameOver;
+
+    #region Singleton
+    public GameManager Instance;
+    private void Awake() {
+        if(Instance!=null){
+            Destroy(gameObject);
+        }else{
+            //DontDestroyOnLoad()
+            Instance=this;
+        }
+    }
+    #endregion
+
+    private void Start(){
+
+        gameOverPanel.SetActive(false);
+        pausePanel.SetActive(false);
+    }
+
+    public void GameOver(){
+        gameOver=true;
+        gameOverPanel.SetActive(true);
+    }
+    public void PauseGame(){
+        pausePanel.SetActive(true);
+        Time.timeScale =0;
+    }
+    public void ResumeGame(){
+        pausePanel.SetActive(false);
+        Time.timeScale =1;
+    }
+    public void RestartGame(){
+        SceneManager.LoadScene(0);
+    }
+
 }
